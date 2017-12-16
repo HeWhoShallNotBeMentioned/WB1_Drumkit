ad1fbf4273be8b343f6f260071907f74b6d1d48c
Chris Underwood <cunderwoodmn@gmail.com>
2017-12-15 22:28:08 -0800

    play the sound and activate the playing css

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 
 window.addEventListener('keydown', function(e){
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  if(!audio) return;
-  console.log(audio);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return;//stops function if the key pressed does not represent an audio file
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("playing");
+  console.log(key);
 });
+
+const keys = document.querySelectorAll(".key");
